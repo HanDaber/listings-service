d40083aeb3b0a8335387f50423980a8e1f9d180a
Dan Haber <handaber@gmail.com>
2012-10-31 22:24:03 -0700

    add jsonp support in server.js

--- server.js
+++ server.js
@@ -61,12 +61,12 @@ var listingSchema = new Schema( listing_properties );
 var listingModel = mongoose.model( 'Listing', listingSchema );
 
 
-// Enable cross domain requests
-app.all('/api', function(req, res, next) {
+// Enable cross domain requests ?
+/*app.all('/api', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+});*/
 
 
 app.get('/', routes.index);
