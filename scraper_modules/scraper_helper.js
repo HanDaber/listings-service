3eb20abb24e8e6bf5bcde01af2bbcf79873d76e6
Dan Haber <handaber@gmail.com>
2013-02-07 12:34:04 -0800

    roughly working, intervaled scrape and web update

---
+++ scraper_modules/scraper_helper.js
@@ -0,0 +1,20 @@
+function leading_zero ( n ) {
+
+    if ( n < 10 ) return '0' + n;
+
+    else return n;
+}
+
+exports.right_now = function () {
+
+    var now = new Date();
+    
+    return ( now.getFullYear() + '-' + leading_zero( now.getMonth() + 1 ) + '-' + leading_zero( now.getDate() ) + 'T' + leading_zero( now.getHours() > 12 ? now.getHours() - 12 : now.getHours() ) + ':' + leading_zero( now.getMinutes() ) + ':' + leading_zero( now.getSeconds() ) + '-08:00' );
+};
+
+exports.a_month_ago = function () {
+
+    var now = new Date();
+    
+    return ( now.getFullYear() + '-' + leading_zero( now.getMonth() ) + '-' + leading_zero( now.getDate() ) + 'T' + leading_zero( now.getHours() > 12 ? now.getHours() - 12 : now.getHours() ) + ':' + leading_zero( now.getMinutes() ) + ':' + leading_zero( now.getSeconds() ) + '-08:00' );
+};
