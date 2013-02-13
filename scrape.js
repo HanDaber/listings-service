adc47f4c3d1781cf6cdd042942268e1877def294
Dan Haber <handaber@gmail.com>
2013-02-12 19:57:38 -0800

    --amend

--- scrape.js
+++ scrape.js
@@ -1,5 +1,10 @@
-var Scraper = require('./scraper_modules/scraper');
+var Scraper = require('./scraper_modules/scraper'),
+	http = require('http');
 
 Scraper.scrape(function ( results ) {
-	console.dir( results.toString() );
+	console.dir( '...scraped...' );
+
+	setTimeout(function () {
+	    http.request("http://listings-service.herokuapp.com", function ( response ) { console.log("req'd"); });
+	}, 1000);
 });
