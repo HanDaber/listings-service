d46c33f576139b4251885b77f909fa89de23db50
Dan Haber <handaber@gmail.com>
2013-02-21 11:26:23 -0800

    --amend

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post( '/update/:user_id', UserManager.update_email );
 
 
 http.createServer( app ).listen( app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 
